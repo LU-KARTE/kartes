7e041ac257947839dd29521652af4e3baacdc98c
Mārtiņš Ciekurs <ciekurs.martins@gmail.com>
2021-05-22 13:00:00 +0300

    Test react

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello There!</p>
+        <p>Hello There! Test here.</p>
         {/*<img src={logo} className="App-logo" alt="logo" />*/}
         {/*<p>*/}
         {/*  Edit <code>src/App.js</code> and save to reload.*/}
