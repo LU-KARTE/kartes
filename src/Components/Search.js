9fea3b10e2802cda81da10c899781e6a61a5d532
Mārtiņš Ciekurs <ciekurs.martins@gmail.com>
2021-05-29 17:24:32 +0300

    Search & filters draft works

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -11,7 +11,24 @@ function Search() {
     const [error, setError] = useState(null);
     const [isSearchLoaded, setSearchIsLoaded] = useState(false);
     const [searchItems, setSearchItems] = useState([]);
-    const [searchTags, setSearchTags] = useState([]);
+    // it outputs in frontend in the same order as the list is here
+    const allFilters = {
+        "roomTypes" : [
+            "kabinets",
+            "laboratorija",
+            "cits",
+        ],
+        "floors" : [
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+        ]
+    }
+
+    const initval = allFilters;
+    const [searchTags, setSearchTags] = useState(initval )
 
     const [searchTerm, setSearchTerm] = React.useState("");
     const [searchResults, setSearchResults] = React.useState([]);
@@ -24,40 +41,34 @@ function Search() {
     // toggle clicked filter
     const handleTagChange = e => {
         // clicked filter value
-        const newFilterVal = e.currentTarget.getAttribute("data-index");
+        const newFilterName = e.currentTarget.getAttribute("data-filtername");
+        const newFilterType = e.currentTarget.getAttribute("data-filtertype");
 
         // add filter
-        if (!searchTags.includes(newFilterVal)) {
-            setSearchTags(currentSearchTags =>
-                currentSearchTags.concat([newFilterVal])
+        if (!searchTags[newFilterType].includes(newFilterName)) {
+            setSearchTags(current =>
+                {
+                    let result = JSON.parse(JSON.stringify(current));// deep copy; otherwise, does not work
+
+                    result[newFilterType].push(newFilterName);
+
+                    return result;
+                }
             )
-            console.log(e);
-            // e.props.colorScheme("teal");
         }
+        
         // remove filter
         else {
-            setSearchTags(currentSearchTags =>
-                currentSearchTags.filter(item => !newFilterVal.includes(item))
+            setSearchTags(current => {
+                    let result = JSON.parse(JSON.stringify(current)); // deep copy; otherwise, does not work
+
+                    result[newFilterType] = searchTags[newFilterType].filter(item => !newFilterName.includes(item))
+
+                    return result;
+                }
             )
         }
     }
-
-    // it outputs in frontend in the same order as the list is here
-    const allFilters = {
-        "roomTypes" : [
-            "kabineti",
-            "laboratorijas",
-            "citi",
-        ],
-        "floors" : [
-            "1. stāvs",
-            "2. stāvs",
-            "3. stāvs",
-            "4. stāvs",
-            "5. stāvs",
-        ]
-    }
-
 
     // Note: the empty deps array [] means
     // this useEffect will run once
@@ -77,22 +88,41 @@ function Search() {
             )
     }, [])
 
+
+    // search logic
     React.useEffect(() => {
         if (isSearchLoaded) {
             const results = searchItems["features"].filter(feature => {
-                    return feature["properties"]["roomID"].toLowerCase().includes(searchTerm.toLowerCase()) ||
-                        feature["properties"]["roomType"].toLowerCase().includes(searchTerm.toLowerCase());
+                    let stLowered = searchTerm.toLowerCase();
+                    let roomID = feature["properties"]["roomID"].toLowerCase();
+                    let roomType = feature["properties"]["roomType"].toLowerCase();
+                    let roomFloor = feature["properties"]["floor"].toLowerCase();
+
+                    let floorFilter = searchTags["floors"];
+                    let roomTypesFilter = searchTags["roomTypes"];
+
+                    console.log(floorFilter);
+                    console.log(roomFloor);
+                    console.log(floorFilter.includes(roomFloor));
+
+                    let filtersOK = roomTypesFilter.includes(roomType) && floorFilter.includes(roomFloor);
+                    let searchParamsOK = roomID.includes(stLowered) || roomType.includes(stLowered);
+
+                    return filtersOK && searchParamsOK;
+
+                    // return feature["properties"]["roomID"].toLowerCase().includes(searchTerm.toLowerCase()) ||
+                    //     feature["properties"]["roomType"].toLowerCase().includes(searchTerm.toLowerCase());
                 }
             )
 
             setSearchResults(results);
         }
-    }, [searchTerm, searchTags]);
+    }, [searchTerm, searchTags, searchItems]);
 
     return (
         <div className="App">
             <ChakraProvider>
-                <InputGroup w={300}>
+                <InputGroup w={301}>
                     <InputLeftElement
                         pointerEvents="none"
                         children={<Search2Icon color="gray.300" />}
@@ -104,14 +134,18 @@ function Search() {
                         onChange={handleChange}
                     />
                 </InputGroup>
+
+                {/* display filter tags */}
                 <HStack spacing={1} m={1}>
                     {
                         Object.keys(allFilters).map((key) => {
                             return allFilters[key].map(filterItem => {
                                 let color = "";
-                                searchTags.includes(filterItem) ? color = "blue" : color = "gray"
+                                // console.log("HEREE");
+                                // console.log(searchTags);
+                                searchTags[key].includes(filterItem) ? color = "blue" : color = "gray"
                                 return (
-                                    <Tag size="sm" colorScheme={color} key={filterItem} data-index={filterItem}
+                                    <Tag size="sm" colorScheme={color} key={filterItem} data-filtertype={key} data-filtername={filterItem}
                                          onClick={(e) => handleTagChange(e)}>
                                         {filterItem}
                                     </Tag> )
@@ -121,11 +155,23 @@ function Search() {
                 </HStack>
 
                 <div>
-                    <ul>
-                        {searchTags.map((item, key) => (
-                            <li key={"itesm-" +key} >{item}</li>
-                        ))}
-                    </ul>
+
+                    {/*/!* current filter list *!/*/}
+                    {/*<ul>*/}
+                    {/*    {*/}
+                    {/*        Object.keys(searchTags).map((key) => {*/}
+                    {/*            return searchTags[key].map(filterItem => {*/}
+                    {/*                return (*/}
+                    {/*                    <li key={filterItem}>{filterItem}</li>*/}
+                    {/*                )*/}
+                    {/*            })*/}
+                    {/*        })*/}
+                    {/*    }*/}
+                    {/*</ul>*/}
+                    {/*<div>&nbsp;</div>*/}
+
+
+                    {/* search results list */}
                     <ul>
                         {searchResults.map((item, key) => (
                             <li key={"item-" +key} >{item["properties"]["floor"]}. stāvs &nbsp; {item["properties"]["roomID"]}.{item["properties"]["roomType"]}</li>
