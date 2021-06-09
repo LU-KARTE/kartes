e506d5c6733b63541aee23425f5b929b1956d5b7
Mārtiņš Ciekurs <ciekurs.martins@gmail.com>
2021-06-09 20:20:14 +0300

    Search kinda finished

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,8 +1,22 @@
 import React, {useEffect, useState} from "react";
 import {
     Badge,
-    Box, Center,
-    ChakraProvider, Flex, HStack, Image, Input, InputGroup, InputLeftElement, List, ListItem, Spacer, Tag, Text
+    Box, Button,
+    Center,
+    ChakraProvider, Fade,
+    Flex,
+    HStack,
+    Image,
+    Input,
+    InputGroup,
+    InputLeftElement,
+    List,
+    ListItem,
+    Spacer,
+    Tag,
+    Text,
+    useDisclosure,
+    ScaleFade, Slide, SlideFade, Collapse, Divider
 } from "@chakra-ui/react";
 import {Search2Icon, StarIcon} from '@chakra-ui/icons'
 import {Link} from "react-router-dom";
@@ -18,6 +32,11 @@ function Search() {
     const [isSearchLoaded, setSearchIsLoaded] = useState(false);
     const [displayResultsList, setDisplayResultsList] = useState("none"); // none or block
     const [searchItems, setSearchItems] = useState([]);
+
+    const [showAllFilters, setShowAllFilters] = React.useState(false)
+
+    const handleToggleFilters = () => setShowAllFilters(!showAllFilters)
+
     // it outputs in frontend in the same order as the list is here
     const allFilters = {
         "roomTypes" : [
@@ -100,7 +119,7 @@ function Search() {
                 }
             )
         }
-        
+
         // remove filter
         else {
             setSearchTags(current => {
@@ -139,10 +158,9 @@ function Search() {
         });
     }, [])
 
-
     // to indicate how good the result is (depends on specific feature properties and search terms splitted)
     function assignPoints(feature) {
-        let stLowered = searchTerm.toLowerCase().replace(/[^a-z0-9 ]/gi,''); // search term whole
+        let stLowered = searchTerm.toLowerCase().replace(/[^a-z0-9 ]/gi,'').trim(); // search term whole
         let roomID = feature["properties"]["roomID"].toLowerCase();
         let roomType = feature["properties"]["roomType"].toLowerCase();
         let roomFloor = feature["properties"]["floor"].toLowerCase();
@@ -154,7 +172,9 @@ function Search() {
 
         let filtersOK = roomTypesFilter.includes(roomType) && floorFilter.includes(roomFloor);
 
-        if (!filtersOK) return 0;
+        if (!filtersOK) {
+            return 0;
+        }
 
         // award points to the potential search result
         let includesPoints = 0.3;
@@ -162,7 +182,9 @@ function Search() {
         let points = 0;
         let searchWordsLength = searchWords.length;
 
-        if (searchWordsLength === 0 || (searchWordsLength === 1 && searchWords[0] === "")) return 1;
+        if (searchWordsLength === 0 || (searchWordsLength === 1 && (searchWords[0] === "" || searchWords[0] === " "))) {
+            return 1;
+        }
 
         for (let i = 0; i < searchWordsLength; i++) {
             if (searchWords[i] === "")
@@ -177,24 +199,30 @@ function Search() {
                 points += includesPoints;
         }
 
-
         return points;
     }
 
     // search logic
     React.useEffect(() => {
         if (isSearchLoaded) {
-            const itemsToSort = JSON.parse(JSON.stringify(searchItems["features"]));
-            const results = itemsToSort.sort((feature1, feature2) => {
-                    let points1 = assignPoints(feature1); // points indicate how "suitable" the result is
-                    let points2 = assignPoints(feature2);
+            const itemsToSort = JSON.parse(JSON.stringify(searchItems["features"])); // deep copy
+            let results = itemsToSort
+                .filter(feature => {
+                    return assignPoints(feature) > 0;
+                })
+                .sort((feature1, feature2) => {
+                        let points1 = assignPoints(feature1); // points indicate how "suitable" the result is
+                        let points2 = assignPoints(feature2);
 
-                    if (points1 > points2) return -1;
-                    else if (points1 < points2) return 1;
-                    else return 0;
+                        if (points1 > points2) return -1;
+                        else if (points1 < points2) return 1;
+                        else return 0;
+                    }
+                )
 
-                }
-            )
+            // #999872 not good. for bug fix that 1 element does not appear (state is not changed somewhy).
+            if (results.length === 1)
+                results.push({});
 
             setSearchResults(results);
         }
@@ -203,50 +231,65 @@ function Search() {
     return (
         <Box id="searchWrapper" width="full" className="App" onFocus={() => setDisplayResultsList("block")}>
                 <SearchInputField searchTerm={searchTerm} handleChange={handleChange}/>
-
-
                 <div>
-
                     {/* search results list */}
                     {/*<Text mt={3} mb={3}><b>Atrastās telpas:</b></Text>*/}
                     <List spacing={0} shadow={"md"} style={{display: displayResultsList}}>
-                        <ListItem key="filterTags" pt={1}>
-                            {/* display filter tags */}
-                            <Flex css={{
-                                flexFlow: "row wrap"
-                            }} m={1}>
-                                {
-                                    Object.keys(allFilters).map((key) => {
-                                        return allFilters[key].map(filterItem => {
 
-                                            // if filter is active, set color to blue; otherwise, gray
-                                            let color = "";
+                        {/* display filter tags */}
+                        <ListItem key="filterTags" p={3} pb={0}>
+                            <Button size="xs" onClick={handleToggleFilters}>Iestatīt filtrus</Button>
+                            <Collapse startingHeight={0} in={showAllFilters}>
+                                <Flex css={{
+                                    flexFlow: "row wrap"
+                                }}>
+                                    {
+                                        // iterate over all tags
+                                        Object.keys(allFilters).map((key) => {
+                                            return allFilters[key].map(filterItem => {
 
-                                            searchTags[key].includes(filterItem["filterTerm"]) ? color = "blue" : color = "gray"
+                                                // if filter is active, set color to blue; otherwise, gray
+                                                let color = "";
 
-                                            return (
-                                                <Tag m={1} size={"lg"} colorScheme={color} key={filterItem["filterTerm"]} data-filtertype={key} data-filtername={filterItem["filterTerm"]}
-                                                     onClick={(e) => handleTagChange(e)}>
-                                                    {filterItem["frontendName"]}
-                                                </Tag> )
+                                                searchTags[key].includes(filterItem["filterTerm"]) ? color = "blue" : color = "gray"
+
+                                                return (
+                                                    <Tag mr={1} mt={1} size={"md"} colorScheme={color} key={filterItem["filterTerm"]} data-filtertype={key} data-filtername={filterItem["filterTerm"]}
+                                                         onClick={(e) => handleTagChange(e)}>
+                                                        {filterItem["frontendName"]}
+                                                    </Tag> )
+                                            })
                                         })
-                                    })
-                                }
-                            </Flex>
+                                    }
+                                </Flex>
+
+                            </Collapse>
                         </ListItem>
-                        {searchResults.slice(0, MAXSEARCHLISTROWS).map((item, key) => (
-                            <Link key={key}  to={"/"+item["properties"]["roomID"]}>
-                                <ListItem _hover={{ bg: "#f1f1f1" }} p={3}>
-                                    <Flex>
-                                        <Text fontSize={"md"}>
-                                            {item["properties"]["roomID"] + ". " + item["properties"]["roomType"]}
-                                        </Text>
-                                        <Spacer />
-                                        <Text>{item["properties"]["floor"]}. stāvs</Text>
-                                    </Flex>
-                                </ListItem>
-                            </Link>
-                        ))}
+
+                        {/* Display search results */}
+                        {searchResults.length > 1 ?
+                            // (Object.keys(searchResults[1]).length === 0 ? 1 : MAXSEARCHLISTROWS) ==> this could be just MAXSEARCHLISTROWS but due #999872 it is so that does not break if only one result found.
+                            searchResults.slice(0, (Object.keys(searchResults[1]).length === 0 ? 1 : MAXSEARCHLISTROWS)).map((item, key) => (
+                                <Link key={key}  to={"/"+item["properties"]["roomID"]}>
+                                    <ListItem _hover={{ bg: "#f1f1f1" }} p={3}>
+                                        <Flex>
+                                            <Text fontSize={"md"}>
+                                                {item["properties"]["roomID"] + ". " + item["properties"]["roomType"]}
+                                            </Text>
+                                            <Spacer />
+                                            <Text>{item["properties"]["floor"]}. stāvs</Text>
+                                        </Flex>
+                                    </ListItem>
+                                    <Divider />
+                                </Link>
+                            ))
+                            :
+                            <ListItem>
+                                <Text fontSize={"md"} p={3}>
+                                    Nekas netika atrasts.
+                                </Text>
+                            </ListItem>
+                        }
                     </List>
                     {/*<li key={"item-" +key} >{item["properties"]["floor"]}. stāvs &nbsp; {item["properties"]["roomID"]}.{item["properties"]["roomType"]}</li>*/}
                 </div>
